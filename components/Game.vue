7411a6a14bcd2d5076139d9b0857dc292c4827fc
Samuel Snopko <samuel@snopko.com>
2020-07-08 16:46:30 +0200

    created game component and game teaser preview page

---
+++ components/Game.vue
@@ -0,0 +1,20 @@
+<template>
+  <a
+    class="inline-block"
+    :href="blok.link.url">
+    <img
+      class="block rounded"
+      :src="blok.cover_image.filename" alt="">
+  </a>
+</template>
+
+<script>
+export default {
+  props: {
+    blok: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
--- plugins/components.js
+++ plugins/components.js
@@ -1,4 +1,6 @@
 import Vue from 'vue'
 import Banner from '~/components/Banner.vue'
+import Game from '~/components/Game.vue'
 
 Vue.component('banner', Banner)
+Vue.component('game', Game)
